78c436de970b086a6e6f5c81579906bfcf17f70f
vilicvane <i@vilic.info>
2017-02-26 15:15:29 +0800

    extend code base for other platforms in the future

--- src/core/china-routes.ts
+++ src/core/china-routes.ts
@@ -1,11 +1,12 @@
 import * as FS from 'fs';
 import * as Path from 'path';
 
+import { ExpectedError } from 'clime';
 import * as v from 'villa';
 
 import { getChinaRoutes, renderTemplate } from '../util';
 
-const TEMPLATES_DIR = Path.join(__dirname, '../../res/windows/script-templates');
+const TEMPLATES_DIR = Path.join(__dirname, `../../res/${process.platform}/script-templates`);
 
 export interface VPNScriptOptions {
   entry: string;
@@ -19,6 +20,12 @@ export interface VPNScriptOptions {
 export type VPNScriptsGeneratingProgressHandler = (state: 'fetching' | 'generating') => void;
 
 export async function generateVPNScripts(options: VPNScriptOptions, progress: VPNScriptsGeneratingProgressHandler): Promise<string[]> {
+  try {
+    await v.call(FS.stat, TEMPLATES_DIR);
+  } catch (error) {
+    throw new ExpectedError(`Current platform "${process.platform}" is not supported`);
+  }
+
   progress('fetching');
 
   let routes = await getChinaRoutes();
@@ -27,7 +34,7 @@ export async function generateVPNScripts(options: VPNScriptOptions, progress: VP
 
   let data = Object.assign({ routes }, options);
 
-  let scriptNames = ['vpn-up.bat', 'vpn-down.bat'];
+  let scriptNames = await v.call(FS.readdir, TEMPLATES_DIR);
 
   return v.map(scriptNames, async scriptName => {
     let templatePath = Path.join(TEMPLATES_DIR, scriptName);
@@ -35,6 +42,10 @@ export async function generateVPNScripts(options: VPNScriptOptions, progress: VP
     let content = await renderTemplate(templatePath, data);
 
     await v.call(FS.writeFile, targetPath, content);
+
+    if (process.platform !== 'win32') {
+      await v.call(FS.chmod, targetPath, 0o755);
+    }
 
     return Path.relative('.', targetPath);
   });
