bc8b28eea18c9d926f403fd91ca52534a6cc1d69
vilicvane <i@vilic.info>
2017-02-26 15:44:18 +0800

    reduce routes size by combining adjacent records

--- src/util/apnic.ts
+++ src/util/apnic.ts
@@ -7,20 +7,55 @@ export interface Route {
   mask: string;
 }
 
+interface RawRoute {
+  networkStr: string;
+  network: number;
+  count: number;
+}
+
 export async function getChinaRoutes(): Promise<Route[]> {
   let response = await fetch(APNIC_URL);
   let text = await response.text();
-  let chinaRouteRegex = /^apnic\|CN\|ipv4\|.+/mg;
-  return (text.match(chinaRouteRegex) || [])
-    .map(line => {
-      let parts = line.split('|');
-      return {
-        network: parts[3],
-        mask: convertIPv4IntegerToString(~(Number(parts[4]) - 1))
-      };
+  let lines = text.match(/^apnic\|CN\|ipv4\|.+/mg) || [];
+
+  let routes: RawRoute[] = [];
+
+  for (let line of lines) {
+    let parts = line.split('|');
+    let networkStr = parts[3];
+    let network = convertIPv4StringToInteger(networkStr);
+    let count = Number(parts[4]);
+
+    if (routes.length) {
+      let lastRoute = routes[routes.length - 1];
+      let lastAddressEnd = lastRoute.network + lastRoute.count;
+      if (lastAddressEnd === network) {
+        lastRoute.count += count;
+        continue;
+      }
+    }
+
+    routes.push({
+      networkStr,
+      network,
+      count
     });
+  }
+
+  return routes.map(route => {
+    return {
+      network: route.networkStr,
+      mask: convertIPv4IntegerToString(~(route.count - 1))
+    };
+  });
 }
 
-function convertIPv4IntegerToString(value: number): string {
-  return `${(value >>> 24)}.${value >>> 16 & 0xff}.${value >>> 8 & 0xff}.${value & 0xff}`;
+function convertIPv4StringToInteger(address: string): number {
+  return address.split('.')
+    .map((part, index) => Number(part) << 24 - index * 8 >>> 0)
+    .reduce((sum, value) => sum + value);
+}
+
+function convertIPv4IntegerToString(address: number): string {
+  return `${(address >>> 24)}.${address >>> 16 & 0xff}.${address >>> 8 & 0xff}.${address & 0xff}`;
 }
